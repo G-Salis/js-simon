9344126e75571531afe1dc8a556757ac89cbd81b
G-Salis <giuseppesalislavoro@gmail.com>
2021-11-09 18:03:18 +0100

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,22 +1,72 @@
 
 const container = document.querySelector('.mycontainer');
 const numMemory = [];
-const myNum = [];
+let myNum = 0;
+const correctNum = [];
+let match = false;
+
+console.log('prova: ', correctNum);
 
 stampNum();
 console.log('out', numMemory)
-setTimeout(record, 3000);
-setTimeout(memoryNum, 3100);
+setTimeout(record, 1000);
+setTimeout(memoryNum, 1200);
+// setTimeout(result, 1300);
 
 
 
+// function result() {
+//   for (let i = 0; i < numMemory.length; i++) {
+
+//     if (numMemory.includes(myNum)) {
+
+//       correctNum.push(myNum)
+//       console.log('correctNum out: ', correctNum);
+
+//     }
+//   }
+  
+
+// }
 
 
 function memoryNum() {
 
   for (let i = 0; i < 5 ; i++) {
-    myNum[i] = parseInt(prompt('Digita i numeri che sono apparsi'))
-    console.log('out', myNum)
+    myNum = parseInt(prompt('Digita i numeri che sono apparsi'))
+    console.log('out', myNum);
+
+    // if (numMemory.includes(myNum))  {
+
+    //   correctNum.push(myNum)
+    //   console.log('correctNum out: ', correctNum);
+
+    //   if (correctNum.length === 0) {
+
+    //     container.innerHTML = `<div>Non hai indovinato alcun numero</div>`
+
+      
+    //   }else{
+
+    //     container.innerHTML = `<div>Hai indovinato ${correctNum.length} numeri</div>`
+
+    //   }
+    // }
+
+    if (numMemory.includes(myNum))  {
+
+      correctNum.push(myNum)
+      console.log('correctNum out: ', correctNum);
+      container.innerHTML = `<div>Hai indovinato ${correctNum.length} numeri</div>`
+    }
+
+      if (correctNum.length === 0) {
+
+        container.innerHTML = `<div>Non hai indovinato alcun numero</div>`
+
+      
+      }
+   
   }
   
 }
